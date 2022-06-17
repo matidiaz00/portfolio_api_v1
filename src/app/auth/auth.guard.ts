14260448355ca8d12bb047d74efe4ba72047a2ae
Matias Diaz <mdiaz@alquilando.com>
2022-06-17 20:06:28 -0300

    Problema al compilar

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,4 +1,5 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { DecodedIdToken } from 'firebase-admin/lib/auth/token-verifier';
 import { FirebaseAdmin, InjectFirebaseAdmin } from 'nestjs-firebase';
 import { Observable } from 'rxjs';
@@ -10,6 +11,7 @@ export class AuthGuard implements CanActivate {
 
     constructor(
         @InjectFirebaseAdmin() private readonly firebase: FirebaseAdmin,
+        private _config: ConfigService,
     ) {
         this.auth = this.firebase.auth;
     }
@@ -24,9 +26,15 @@ export class AuthGuard implements CanActivate {
                 if (token != null && token != '') {
                     this.auth.verifyIdToken(token)
                         .then((decodedToken: DecodedIdToken) => {
-                            if (decodedToken.uid == process.env.FIREBASE_USER_UID) resolve(true)
-                            else {
-                                console.error(`The user ${decodedToken.email} not has permission for application.`);
+                            const FIREBASE_USER_UID = this._config.get<string>('FIREBASE_USER_UID');
+                            if (FIREBASE_USER_UID) {
+                                if (decodedToken.uid == FIREBASE_USER_UID) resolve(true)
+                                else {
+                                    console.error(`The user ${decodedToken.email} not has permission for application.`);
+                                    reject(false)
+                                }
+                            } else {
+                                console.error(`Error when read .env file and get FIREBASE_USER_UID, this return: `, FIREBASE_USER_UID);
                                 reject(false)
                             }
                         }).catch((err: Error) => {
