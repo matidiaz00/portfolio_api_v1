136b7718ccbbc773619f722558d83af2e23c1395
Matias Diaz <mdiaz@alquilando.com>
2022-08-26 21:19:15 -0300

    ci/cd configurations and testing

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-export const preset = 'ts-jest';
-export const testEnvironment = 'node';
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+};
